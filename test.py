fff19442e7965448ef205c56d1856b5976c0f7a3
Carly Hoover <carly.hoover@slu.edu>
2024-10-11 11:42:01 -0500

    More GUI Horsin' Around

--- test.py
+++ test.py
@@ -1,10 +1,37 @@
-from PyQt5.QtWidgets import QApplication, QWidget
 import sys
 
-##only need one QApplication per file 
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.buttonClicked)
+        button.clicked.connect(self.buttonToggled)
+
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+        
+        ##also consider .setMinimumSize() as well as .setMaximumSize()
+        self.setFixedSize(QSize(400,300))
+
+    def buttonClicked(self):
+        print("Clicked")
+            
+    def buttonToggled(self, checked):
+        print("Checked?", checked)
+
 app = QApplication(sys.argv)
 
-window = QWidget()
-window.show() ##windows are hidden by default
+window = MainWindow()
+window.show()
 
 app.exec()
