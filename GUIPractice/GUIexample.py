3f4dc1677b7187b970408d28dfae7e4c33d3831b
Carly Hoover <carly.hoover@slu.edu>
2024-10-20 00:34:54 -0500

    Adding Comments

--- GUIPractice/GUIexample.py
+++ GUIPractice/GUIexample.py
@@ -4,16 +4,21 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 import sys
 
 def main(): 
+    ##basic window 
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
     mainWindow.setGeometry(200,200,300,300)
     mainWindow.setWindowTitle("Test Window")
 
+    ##basic text label on window 
     label = QLabel(mainWindow)
     label.setText("Test Label")
     label.move(50,50)
     
+    ##makes window visible 
     mainWindow.show()
     sys.exit(app.exec_())
+    
+    # https://www.techwithtim.net/tutorials/pyqt5-tutorial/basic-gui-application
     
 main()
