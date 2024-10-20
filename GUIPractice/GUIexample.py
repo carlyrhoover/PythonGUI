4421d1049ab3c1259e2439c3cc34b3ff89fe4a32
Carly Hoover <carly.hoover@slu.edu>
2024-10-20 00:57:57 -0500

    Updated button and label

--- GUIPractice/GUIexample.py
+++ GUIPractice/GUIexample.py
@@ -1,24 +1,46 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 import sys
 
-def main(): 
-    ##basic window 
-    app = QApplication(sys.argv)
-    mainWindow = QMainWindow()
-    mainWindow.setGeometry(200,200,300,300)
-    mainWindow.setWindowTitle("Test Window")
+class MyWindow(QMainWindow): 
+    def __init__(self):  # Fixed the typo here
+        super(MyWindow, self).__init__()
+        self.initUI()
+        
+    ##the button will remain clicked until otherwise 
+    def button_clicked(self):
+        print("Button has been clicked.")
+        self.label.setText("The button has been pressed.")
+        self.update()
+        
+    def update(self):
+        self.label.adjustSize()
+        
+    def initUI(self):
+        # Basic window settings
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Window Test")
 
-    ##basic text label on window 
-    label = QLabel(mainWindow)
-    label.setText("Test Label")
-    label.move(50,50)
-    
-    ##makes window visible 
-    mainWindow.show()
+        # Basic text label on the window
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Test Label")
+        self.label.move(50, 50)
+        
+        # Basic button example
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText("Click Button!")
+        self.button.move(50, 100)  # Added move to position the button
+        self.button.clicked.connect(self.button_clicked)
+
+# Main function to run the window
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
     sys.exit(app.exec_())
+
+window()
+        
+# https://www.techwithtim.net/tutorials/pyqt5-tutorial/basic-gui-application
     
-    # https://www.techwithtim.net/tutorials/pyqt5-tutorial/basic-gui-application
-    
-main()
